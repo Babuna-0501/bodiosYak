a8b1f328a53ac411440314068a2a907866f3ddc9
Babuna-0501 <74554961+Babuna-0501@users.noreply.github.com>
2024-11-10 16:07:56 +0800

    added animation

--- src/components/BodiosYakDown.jsx
+++ src/components/BodiosYakDown.jsx
@@ -1,34 +1,37 @@
 import React from 'react';
 import YakTextWrapper from './YakText/YakTextWrapper';
+import './float.css'
 
 function BodiosYakDown() {
   return (
     <main className="mt-10 w-full text-center uppercase text-[330px] text-stone-800 max-md:mt-10 max-md:max-w-full max-md:text-4xl">
-    <div className='relative'>
-      <h1>
-        bodios
-        <br />
-        yak down
-      </h1>
+      <div className="relative">
+        <h1>
+          bodios
+          <br />
+          yak down
+        </h1>
         <img
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/70cf1b479d9f4079bbe408daa92d272b75f0b3c725771b1b2ff78c9487870434?placeholderIfAbsent=true&apiKey=b9b4bb420cb64e36b6b71e99a06536e1"
-          alt="Decorative image"
+          alt="Decorative"
           loading="lazy"
-          className="object-contain w-full aspect-square max-w-[364px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          className="object-contain w-full aspect-square max-w-[364px] absolute top-1/3 left-[43%] -translate-x-1/2 animation-float"
         />
-    </div>
+
+
+
+      </div>
       <section className="image-container">
-      <img
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/9aceb5f505bc58f99903cd9f2f2de15c6205dbf3c049d3d595f1abeca0e952b7?placeholderIfAbsent=true&apiKey=b9b4bb420cb64e36b6b71e99a06536e1"
-        alt="Descriptive image content"
-        className="object-contain w-full aspect-[1.78]"
-        loading="lazy"
-      />
+        <img
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/9aceb5f505bc58f99903cd9f2f2de15c6205dbf3c049d3d595f1abeca0e952b7?placeholderIfAbsent=true&apiKey=b9b4bb420cb64e36b6b71e99a06536e1"
+          alt="Descriptive"
+          className="object-contain w-full aspect-[1.78]"
+          loading="lazy"
+        />
       </section>
-      <YakTextWrapper/>
+      <YakTextWrapper />
     </main>
   );
 }
 
 export default BodiosYakDown;
-
--- src/components/CenterTitle/CenterTitle.jsx
+++ src/components/CenterTitle/CenterTitle.jsx
@@ -13,6 +13,14 @@ function CenterTitle() {
       {textBlocks.map((block, index) => (
         <TextBlock key={index} text={block.text} className={block.className} />
       ))}
+      <section className="image-container">
+        <img
+          src="https://cdn.builder.io/api/v1/image/assets/9804d3a088fa478e8672485264720a14/6df5c2e921975cf53202a065bfd807a53d3af281253386a3915dec1fc6d70fd9?apiKey=9804d3a088fa478e8672485264720a14&"
+          alt="Decorative"  
+          className="object-contain w-full aspect-[2.42]"
+          loading="lazy"
+        />
+      </section>
     </header>
   );
 }
