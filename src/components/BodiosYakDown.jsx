a989b96072cc5745c540f4a000e352fcded3957d
Babuna-0501 <74554961+Babuna-0501@users.noreply.github.com>
2024-11-13 18:50:06 +0800

    d

--- src/components/BodiosYakDown.jsx
+++ src/components/BodiosYakDown.jsx
@@ -6,20 +6,19 @@ function BodiosYakDown() {
   return (
     <main className="mt-10 w-full text-center uppercase text-[330px] text-stone-800 max-md:mt-10 max-md:max-w-full max-md:text-4xl">
       <div className="relative">
-        <h1>
+        <h1 class="text-5xl sm:text-6xl md:text-7xl lg:text-[330px]">
           bodios
           <br />
           yak down
         </h1>
-        <img
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/70cf1b479d9f4079bbe408daa92d272b75f0b3c725771b1b2ff78c9487870434?placeholderIfAbsent=true&apiKey=b9b4bb420cb64e36b6b71e99a06536e1"
-          alt="Decorative"
-          loading="lazy"
-          className="object-contain w-full aspect-square max-w-[364px] absolute top-1/3 left-[43%] -translate-x-1/2 animation-float"
-        />
-
-
-
+        <div class="absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/3 lg:block hidden">
+          <img
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/70cf1b479d9f4079bbe408daa92d272b75f0b3c725771b1b2ff78c9487870434?placeholderIfAbsent=true&apiKey=b9b4bb420cb64e36b6b71e99a06536e1"
+            alt="Decorative"
+            loading="lazy"
+            class="object-contain w-3/4 sm:w-full sm:max-w-[364px] aspect-square max-w-full animation-float"
+          />
+        </div>
       </div>
       <section className="image-container">
         <img
