5acf4a041ccf3302a47fdec12a96ebb88d8f152d
Babuna-0501 <74554961+Babuna-0501@users.noreply.github.com>
2025-01-09 14:45:43 +0800

    update

--- src/components/Artisan/ArtisanContent.jsx
+++ src/components/Artisan/ArtisanContent.jsx
@@ -3,14 +3,11 @@ import { CallToAction } from "./CallToAction";
 
 export function ArtisanContent() {
   return (
-    <div className="flex relative flex-col mb-0 max-w-full w-full px-5 md:px-10 lg:px-20 py-4">
-      {/* Title */}
-      <div className="self-start ml-0 text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center font-bold">
+    <div className="flex relative flex-col mb-0 max-w-full w-[1121px] max-md:mb-2.5 py-6">
+      <div className="self-start ml-5 text-5xl text-center max-md:max-w-full max-md:text-4xl">
         FROM A KNIT COMMUNITY OF MONGOLIAN <br />
         ARTISANS TO YOU
       </div>
-
-      {/* Content Text */}
       <div className="mt-8 md:mt-10 leading-7 text-justify font-libreFranklin text-[12px] sm:text-[12px] md:text-xl lg:text-2xl">
         BODIOS, a distinguished family-owned Mongolian brand with 30 years
         of heritage, is now proudly led by its second generation. With
@@ -21,8 +18,6 @@ export function ArtisanContent() {
         timeless pieces that resonate with refined taste and uncompromising
         quality...
       </div>
-
-      {/* Call to Action */}
       <CallToAction />
     </div>
   );
